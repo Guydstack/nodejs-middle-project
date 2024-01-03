48f2752fcc4fe09cc3b3a01a18d17015687753cb
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-03 10:49:53 +0200

    initial commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-console.log('MongoDB URI:', process.env.DATABASE_URL);
 
 const uri = process.env.DATABASE_URL;
 
 async function main() {
   try {
-   await mongoose.connect(uri)
+   await mongoose.connect(process.env.DATABASE_URL)
    console.log("mongoose Connected!")
   } catch (error) {
     console.log(error)

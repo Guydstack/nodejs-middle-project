89a2c165f9c8e90729ecb3c91b8aa453194297a0
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-06 15:17:21 +0200

    initial commit

--- controller/client_controller.js
+++ controller/client_controller.js
@@ -90,7 +90,6 @@ module.exports = {
   const token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:1000 * 60 * 60});
       
 res.cookie("token", token, {
-  sameSite: "Lax",
   domain: '.onrender.com',
   path: '/', // Cookie will be sent for all paths
 });

66058fbad6d8b61a04e87d613e3666f3893fbc6a
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-06 12:15:57 +0200

    initial commit

--- controller/client_controller.js
+++ controller/client_controller.js
@@ -90,7 +90,8 @@ module.exports = {
   const token = jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:1000 * 60 * 60});
       
 res.cookie("token", token, {
-  sameSite: "Lax", // Allow cross-origin cookies
+  sameSite: "None", // Allow cross-origin cookies
+  secure: true, // Requires HTTPS
 });
 console.log('Cookie set:', token);
 

96c124609e996f1d04cd5850e054a4aab7954647
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-06 14:43:43 +0200

    initial commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "client")));
 app.use(express.static(path.join(__dirname, "public/uploads")));
 app.use(cookieParser());
 //העברנו את הבקשה במידל וואר שמטפל בבקשות מהשרת

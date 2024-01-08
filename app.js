d71ad1f5d27d573799058aef1014b094a1bef89e
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-08 17:03:16 +0200

    initial commit

--- app.js
+++ app.js
@@ -20,10 +20,7 @@ const workers_router = require("./routes/workers");
 const events_router = require("./routes/events");
 const orders_router = require("./routes/orders");
 
-app.use(cors({
-  origin: /\.onrender\.com$/, // Allows all subdomains under onrender.com
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client")));
